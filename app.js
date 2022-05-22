37ff61749576d422751cee8d2fff029e8f157f50
Rohan Goyal <rohangoyal991@gmail.com>
2022-05-22 12:55:04 +0530

    Mistyped procees.env.DB_URL in app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
 
-const dbUrl = process.env.dbUrl || 'mongodb://localhost:27017/yelp-camp'
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'
 
 
 mongoose.connect(dbUrl, {
